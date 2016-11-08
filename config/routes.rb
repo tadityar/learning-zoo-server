d26e10454886654a4a851ea058097847d475bd25
Tasya Aditya Rukmana <tadityar@gmail.com>
2016-11-08 23:29:17 +0800

    Add TopicsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   root "home#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :topics do
+    get :incr_counter, on: :member
+  end
 end
