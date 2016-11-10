fabc5c1d2d0f2908f281642eb14e0b1e779f32ff
Tasya Aditya Rukmana <tadityar@gmail.com>
2016-11-10 14:04:03 +0800

    Remove subject validation

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session < ActiveRecord::Base
   has_many :topics, inverse_of: :sessions
-  belongs_to :subjects, inverse_of: :sessions
+  belongs_to :subject, inverse_of: :sessions
 
-  validates :name, :lecturer_name, :current_slide, :subject, presence: true
+  validates :name, :lecturer_name, :current_slide, presence: true
 end
