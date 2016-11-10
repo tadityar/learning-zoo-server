0a9cc43619438db7b03f70a8516fdc756d13191f
Tasya Aditya Rukmana <tadityar@gmail.com>
2016-11-10 14:18:48 +0800

    Remove several sesssion validations

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,5 +2,5 @@ class Session < ActiveRecord::Base
   has_many :topics, inverse_of: :sessions
   belongs_to :subject, inverse_of: :sessions
 
-  validates :name, :lecturer_name, :current_slide, presence: true
+  validates :name, presence: true
 end
